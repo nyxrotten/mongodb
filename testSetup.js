2fc4d26cfd763ed8233371f9af4bc0207f83413a
Antonio Llamas <antoniollamasln@gmail.com>
2024-05-28 19:24:38 +0200

    fix test setup

--- testSetup.js
+++ testSetup.js
@@ -1,13 +1,46 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
+const Book = require('./src/book')
 
-const Book = require('./src/book');
+const db = 'practice-mongodb'
+const url = 'mongodb://127.0.0.1:27017/'
 
-const db = 'practice-mongodb';
-const url = 'mongodb://127.0.0.1:27017/';
+const main = async () => {
+	try {
+		await mongoose.connect(url + db, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+		})
+		console.log('Connected to MongoDB')
+	} catch (err) {
+		console.error('Error connecting to MongoDB:', err)
+	}
+}
 
-const main = async () => await mongoose.connect(url + db);
+beforeAll(async () => {
+	await main()
+})
 
-main().catch(console.err);
+afterAll(async () => {
+	try {
+		await mongoose.connection.close()
+		console.log('MongoDB connection closed')
+	} catch (err) {
+		console.error('Error closing MongoDB connection:', err)
+	}
+})
 
-beforeEach(async () => await Book.deleteMany({}));
-afterEach(async () => await Book.deleteMany({}));
+beforeEach(async () => {
+	try {
+		await Book.deleteMany({})
+	} catch (err) {
+		console.error('Error deleting books:', err)
+	}
+})
+
+afterEach(async () => {
+	try {
+		await Book.deleteMany({})
+	} catch (err) {
+		console.error('Error deleting books:', err)
+	}
+})
