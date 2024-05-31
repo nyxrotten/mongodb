91a1d14d2ac96eb0abd0f4d3acf9e5310f4cd40e
nyxrotten <nes_calina@hotmail.com>
2024-05-31 21:19:18 +0200

    tests 4/5

--- src/book.js
+++ src/book.js
@@ -1,17 +1,17 @@
 const mongoose = require('mongoose');
 
 const bookSchema = new mongoose.Schema({
-	title: String,
-	isbn: String,
-	status: String,
-	pageCount: String,
-	published: Boolean,
+	title: { type: String, required: true },
+	isbn: { type: String, unique: true, required: true  },
+	status: { type: String, required: true },
+	pageCount: {type: Number},
+	published: { type: Boolean, default: true },
 	thumbnailUrl: String,
 	publishedDate: Date,
 	shortDescription: String,
 	longDescription: String,
 	authors: [{ type: String }],
-	categories: { type: String },
+	categories:  [{ type: String }]
 });
 
 const Book = mongoose.model('book', bookSchema);
